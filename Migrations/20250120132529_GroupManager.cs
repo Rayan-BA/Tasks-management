6e65fa385366817f54f5a43464eaf4ab471be502
RaychuW <rb.alhamdan@gmail.com>
2025-01-22 08:40:33 +0300

    migrations

---
+++ Migrations/20250120132529_GroupManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Final_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class GroupManager : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ManagerId",
+                table: "Group",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ManagerId",
+                table: "Group");
+        }
+    }
+}
